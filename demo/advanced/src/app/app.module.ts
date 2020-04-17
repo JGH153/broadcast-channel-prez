5e4c5e737d5f1077dca6f9d7471902d30a80cbab
JGH153 <jan.greger@gmail.com>
2020-04-17 15:47:42 +0200

    Init angular

---
+++ demo/advanced/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardsComponent } from './cards/cards.component';
+import { CardDetailComponent } from './card-detail/card-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardsComponent,
+    CardDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
